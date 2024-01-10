c7e9c7c191d22be4050ae46b4dbf93a24343f8bb
SvetaPro <144365251+SvetaPro@users.noreply.github.com>
2024-01-10 23:57:15 +0500

    Создание базового вида приложения

--- csv_column_definer.py
+++ csv_column_definer.py
@@ -1,15 +1,23 @@
 import tkinter as tk
 from tkinter.scrolledtext import ScrolledText as st
-from tkinter import messagebox as mb
-from tkinter import filedialog as fd 
+from tkinter import filedialog as fd
 import os
 import pandas as pd
+import re
 
+# Обработчик для кнопки
+def process_button():
+    file_name = fd.askopenfilename(initialdir=os.getcwd())
 
-# Создание главного окна
+# Создание графического интерфейса
 window = tk.Tk()
-window.geometry("550x550")
+window.geometry("840x500")
 window.title("Программа анализа столбцов .csv файлов")
 
-# Запуск цикла mainloop 
+output_text = st(height=25, width=100)
+output_text.grid(row=1, column=0, padx=10, pady=10)
+
+button = tk.Button(window, text="Открыть CSV", command=process_button)
+button.grid(row=2, column=0)
+
 window.mainloop()
